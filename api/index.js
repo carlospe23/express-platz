2b9dd756374cbdc72698db522bb10879506d2b71
Carlos Reyes <careyespe26@gmail.com>
2023-07-20 21:33:32 -0500

    vercel config

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const routerApi = require('./routes/index.js')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middleware/errorHandler.js')
 
 const app = express();
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(express.json());
 
