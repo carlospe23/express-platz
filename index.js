6cbfb683dc416bc1f4ef2da71600f9223f79d0d2
Carlos Reyes <careyespe26@gmail.com>
2023-07-20 21:23:19 -0500

    cors config

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors')
 const routerApi = require('./routes/index.js')
 const {logErrors, errorHandler, boomErrorHandler} = require('./middleware/errorHandler.js')
 
@@ -7,10 +8,25 @@ const port = 3000
 
 app.use(express.json());
 
+// const whiteList = ['http://localhost:3000'];
+// const options = {
+//   origin: () => {
+//     origin: (origin, callback) => {
+//       if (whiteList.includes(origin)){
+//         callback(null, true)
+//       } else{
+//         callback(new Error('no permitido'))
+//       }
+//     }
+//   }
+// }
+// app.use(cors(options))
+
 routerApi(app);
 app.use(logErrors);
 app.use(boomErrorHandler)
 app.use(errorHandler);
+
 
 app.listen(port, ()=>{
   console.log('escuchando puerto' + port);
